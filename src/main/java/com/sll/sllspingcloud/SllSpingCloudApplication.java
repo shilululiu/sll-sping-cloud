3ba2462d65af1768953b7f7a6f250b5aaaa268ec
sll <17629581571@163.com>
2020-03-30 10:58:13 +0800

    Initial commit

---
+++ src/main/java/com/sll/sllspingcloud/SllSpingCloudApplication.java
@@ -0,0 +1,13 @@
+package com.sll.sllspingcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SllSpingCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SllSpingCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sll/sllspingcloud/SllSpingCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sll.sllspingcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SllSpingCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
